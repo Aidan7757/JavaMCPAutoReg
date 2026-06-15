c5625fc78afba28b3ac9122bbe08ee86dc2d36c7
Aidan Chadha <aidanchadha03@vt.edu>
2026-06-15 17:36:31 -0400

    initial commit

---
+++ src/main/java/com/aidanchadha/mcp_autoreg/Mcp_autoreg.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.aidanchadha.mcp_autoreg;
+
+/**
+ *
+ * @author aidanchadha
+ */
+public class Mcp_autoreg {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
